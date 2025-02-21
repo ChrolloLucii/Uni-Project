8dd5e3954fbb1b314069e24357c022db71e9640d
levkaflower <levkaflower@gmail.com>
2025-02-21 20:22:37 +0300

    Репо для team

---
+++ backend/domain/repositories/TeamRepository.js
@@ -0,0 +1,17 @@
+export default class TeamRepository {
+	async createTeam(team) {
+		throw new Error('ERR_METHOD_NOT_IMPLEMENTED')
+	}
+
+	async findById(id) {
+		throw new Error('ERR_METHOD_NOT_IMPLEMENTED')
+	}
+
+	async update(team) {
+		throw new Error('ERR_METHOD_NOT_IMPLEMENTED')
+	}
+
+	async delete(id) {
+		throw new Error('ERR_METHOD_NOT_IMPLEMENTED')
+	}
+}
