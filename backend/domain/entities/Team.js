f11cd1419137265c3d7f80877e7c7de75b86b708
jammbeast <jammbeast69@mail.ru>
2025-02-27 11:11:22 +0300

    Добавлен класс Match для представления матчей, обновлен класс Team для поддержки рейтинга, и изменен класс Tournament для учета матчей.

--- backend/domain/entities/Team.js
+++ backend/domain/entities/Team.js
@@ -1,22 +1,10 @@
 
 export default class Team { // Класс Team описывает команду в виде объекта
     
-	constructor({ id, name, players }) {
-		if (!name) {
-			throw new Error('Название команды обязательно.')
-		}
-		if (!Array.isArray(players) || players.length !== 5) {
-			throw new Error('Команда должна состоять ровно из 5 игроков.')
-		}
-		const captainCount = players.filter(
-			player => player.role === 'CAPTAIN' // Проверяем, что у игрока роль капитана
-		).length
-		if (captainCount !== 1) {
-			throw new Error('В команде должен быть ровно один капитан.')
-		}
-
+	constructor({ id, name, rating, players }) {
 		this.id = id
 		this.name = name
 		this.players = players
+		this.rating = rating
 	}
 }
