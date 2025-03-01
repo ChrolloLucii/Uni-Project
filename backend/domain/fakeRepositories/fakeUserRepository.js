e3f5f79c248ede05fcb3a410803a1be36d227a54
jammbeast <jammbeast69@mail.ru>
2025-03-01 04:02:56 +0300

    Добавлен метод удаления пользователя в FakeUserRepository и создан интеграционный тест для полной логики бэкенда, включая регистрацию, обновление и удаление пользователей, а также создание и управление командами и турнирами.

--- backend/domain/fakeRepositories/fakeUserRepository.js
+++ backend/domain/fakeRepositories/fakeUserRepository.js
@@ -1,39 +1,36 @@
-class FakeUserRepository{
+class FakeUserRepository {
     constructor () {
-        this.users = new Map();
-        const superUser = {
-            'id' : 1,
-            nickname : 'organizer',
-            role : 'ORGANIZER',
-            username : 'user88',
-            password : 'securepassword', // должен быть зашифрен
-            email : null
-        };
-        this.users.set(superUser.id, superUser);
+      this.users = new Map();
+      const superUser = {
+          id : 1,
+          nickname : 'organizer',
+          role : 'ORGANIZER',
+          username : 'user88',
+          password : 'securepassword',
+          email : null
+      };
+      this.users.set(superUser.id, superUser);
     }
     async create(user){
-        this.users.set(user.id, user);
-        return user;
+      this.users.set(user.id, user);
+      return user;
     }
     async findById(id){
-        return this.users.get(id);
+      return this.users.get(id);
     }
-
     async findAll(){
-        return Array.from(this.users.values());
+      return Array.from(this.users.values());
     }
-
     async update(user){
-        if (!this.users.has(user.id)){
-            throw new Error('User not found');
-        }
-        else {
+      if (!this.users.has(user.id)){
+        throw new Error('User not found');
+      } else {
         this.users.set(user.id, user);
         return user;
+      }
     }
+    async delete(id) {
+      return this.users.delete(id);
     }
-    async delete(user) {
-        return this.users.delete(user.id);
-    }
-}
-export default FakeUserRepository;
+  }
+  export default FakeUserRepository;
