570c8aedab5083058d773a8c36bc2c0ef4f5162f
jammbeast <jammbeast69@mail.ru>
2025-03-03 15:56:12 +0300

    Перенес форму в отдельную страничку во избежание её пропажи при обновлении

--- frontend/src/app/tournamentCreation/page.js
+++ frontend/src/app/tournamentCreation/page.js
@@ -1,8 +1,8 @@
 "use client"
 import React, {useState, useEffect} from "react"
-
+import {useRouter} from "next/navigation"
 export default function TournamentForm(){
-
+    const router = useRouter();
     const [tournament, setTournament] = useState({
         name: "",
         startDate: "",
@@ -11,10 +11,6 @@ export default function TournamentForm(){
         teams: [],
         judges: []
     });
-
-
-    const [ createdTournament, setCreatedTournament] = useState(null);
-    const [newTeam, setNewTeam] = useState({id: "", name: "", rating: 0, players: []});
     const disciplines = ["dota2", "counterStrike2"];
 
     const handleTournamentChange = (e) => {
@@ -41,55 +37,18 @@ export default function TournamentForm(){
             }
             const data = await res.json();
             console.log("Турнир создан", data);
-            setCreatedTournament(data);
             alert("Турнир создан");
+            router.push('/tournament/${data.id}/teams');
         }
         catch(error){
             console.error(error);
             alert("Что-то пошло не так(разбирайся сам)");
         }
-    };
-        const handleTeamChange = (e) => {
-            const {name, value} = e.target;
-            setNewTeam((prev) =>({
-                ...prev,
-                [name]: value
-            }));
-    };
-
-        const handleAddTeam = async (e) =>{
-            e.preventDefault();
-            if (!createdTournament || !createdTournament.id){
-                alert("Сначала создайте турнир");
-                return;
-            }
-        try{
-            const res = await fetch(`http://localhost:4000/api/tournaments/${createdTournament.id}/teams`,
-                {
-                    method: "POST",
-                    headers: 
-                    {"content-type" : "application/json"},
-                    body: JSON.stringify(newTeam)
-                }
-            );
-            if (!res.ok){
-                throw new Error(`ошибка: ${res.status}`);
-            }
-            const updatedTournament = await res.json();
-            console.log("Команда Добавлена", updatedTournament);
-            setCreatedTournament(updatedTournament);
-            setNewTeam({id : "", name: "", rating: "", players: []});
-        } catch(error){
-            console.error(error);
-            alert("Что-то пошло не так(разбирайся сам)");
-        
-        }
-    };
+      };
 
 
     return (
         <div>
-            {!createdTournament && (
                 <form className ="text-white" onSubmit ={handleTournamentSubmit}>
                     <h2 className=" text-2xl mb-4 text-white>">Создать турнир </h2>
                     <div className= "mb-4">
@@ -141,70 +100,6 @@ export default function TournamentForm(){
                     </button>
 
                 </form>
-            )}
-            {createdTournament && (
-                <div className ="mt-8">
-                    <h2 className = "text-2xl mb-4 text-white"> Добавить команду в турнир {createdTournament.name} </h2>
-                    
-                    <form className="mb-4" onSubmit={handleAddTeam}>
-                        <div className ="mb-4">
-                            <label className="block mb-1 text-white" htmlFor="id"> id</label>
-                            <input
-                            type="text"
-                            id="id"
-                            name="id"
-                            value={newTeam.id}
-                            onChange={handleTeamChange}
-                            className="border border-gray-300 rounded p-2"
-                            required
-                            />
-
-                        </div>
-                        <div className ="mb-4">
-                            <label className="block mb-1 text-white" htmlFor="name"> Название</label>
-                            <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={newTeam.name}
-                            onChange={handleTeamChange}
-                            className="border border-gray-300 rounded p-2"
-                            required
-                            />
-                        </div>
-                        <div className ="mb-4">
-                            <label className="block mb-1 text-white" htmlFor="rating"> Аверага</label>
-                            <input
-                            type="text"
-                            id="rating"
-                            name="rating"
-                            value={newTeam.rating}
-                            onChange={handleTeamChange}
-                            className="border border-gray-300 rounded p-2"
-                            required
-                            />
-
-                        </div>
-                        <button type="submit" className="w-full py-2 px-4 rounded text-white bg-gray-700">
-                        Добавить команду
-                        </button>
-
-                    </form>
-                    
-                    {createdTournament.teams && createdTournament.teams.length > 0 && (
-                        <div>
-                            <h2 className="text-2xl mb-4 text-white">Команды</h2>
-                            <ul>
-                                {createdTournament.teams.map((team, index) => (
-                                    <li key={index} className="text-white">
-                                        {team.name} - {team.rating}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-            )}
-    </div>
-            )}
-            </div>
-  );
+        </div>
+    );
 }
