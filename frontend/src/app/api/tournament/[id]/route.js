c4199da35cc79102f826804887c9fd64958d11b7
jammbeast <jammbeast69@mail.ru>
2025-03-05 15:44:53 +0300

    пофиксил баг с удалением с клиента

--- frontend/src/app/api/tournament/[id]/route.js
+++ frontend/src/app/api/tournament/[id]/route.js
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(request, { params }) {
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:4000' || 'http://example:4000';
-    const { id } = params;
-    console.log("Удаляем турнир с id:", id);
-    const res = await fetch(`${backendUrl}/api/tournaments/${id}`, {
+    const tournamentId  = parseInt(params.id,10);
+    console.log("Удаляем турнир с id:", tournamentId);
+    const res = await fetch(`${backendUrl}/api/tournaments/${tournamentId}`, {
         method: 'DELETE',
     });
     if (!res.ok) {
         return NextResponse.json({ error: 'Error deleting tournament' }, { status: res.status });
     }
-    return NextResponse.json({}, { status: 204 });
+    return NextResponse.json({}, { status: 200 });
 }
